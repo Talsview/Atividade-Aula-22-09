b571150190a340db9450b8d77b044a7a94b3bd08
Talsview <daviconcyline@gmail.com>
2025-10-06 19:12:36 -0300

    Main.java

--- Gerenciar funcionários/Main.java
+++ Gerenciar funcionários/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Funcionario f1 = new Gerente("Carlos", 5000, 2000);
+        Funcionario f1 = new Gerente("Davi", 5000, 2000);
         Funcionario f2 = new Programador("Ana", 3000, 10);
-        Funcionario f3 = new Estagiario("João", 2000);
+        Funcionario f3 = new Estagiario("Caio", 2000);
 
         f1.exibirInformacoes();
-        System.out.println("---------------------");
+        System.out.println();
         f2.exibirInformacoes();
-        System.out.println("---------------------");
+        System.out.println();
         f3.exibirInformacoes();
     }
+
 }
